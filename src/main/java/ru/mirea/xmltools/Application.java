6326a8a19fe0a29435d1789ca709b76491d240b1
bradibor <just1bomb@gmail.com>
2018-12-07 10:25:53 +0300

    editing

--- src/main/java/ru/mirea/xmltools/Application.java
+++ src/main/java/ru/mirea/xmltools/Application.java
@@ -4,6 +4,7 @@ import lombok.val;
 import ru.mirea.xmltools.domain.LegalEntity;
 import ru.mirea.xmltools.repository.LegalEntityRepository;
 
+import java.io.PrintStream;
 import java.util.Optional;
 import java.util.Scanner;
 
@@ -12,19 +13,90 @@ public class Application {
         final Scanner in = new Scanner(System.in);
         System.out.println("enter working dir: ");
         final String workingDir = in.nextLine();
+        final LegalEntityRepository legalEntityRepository = new LegalEntityRepository(workingDir);
         System.out.println("enter ogrn to edit: ");
         final Long ogrn = Optional.of(in.nextLine()).filter(s->!s.isEmpty()).map(Long::valueOf).orElse(null);
 
-        final LegalEntityRepository legalEntityRepository = new LegalEntityRepository("C:\\Users\\bradi\\IdeaProjects\\xmltools\\src\\main\\resources");
-        final LegalEntity legalEntity = new LegalEntity() {{
-            setOgrn(302032341L);
-            setInn("777198");
-            setEntityType(LegalEntity.EntityType.organizaion);
-            setKpp("23409");
-            setStatus(LegalEntity.Status.active);
-        }};
+        final LegalEntity legalEntity;
+        if(ogrn == null) {
+            System.out.println("creating new entry");
+            legalEntity = new LegalEntity();
+        } else {
+            val legalEntityOpt = legalEntityRepository.getByOgrn(ogrn);
+            legalEntity = legalEntityOpt.orElseGet(() -> new LegalEntity() {{
+                System.out.println("legal entity not found - creating new entry");
+                setOgrn(ogrn);
+            }});
+        }
+
+        if(legalEntity.getOgrn() == null) {
+           setOgrn(legalEntity, System.out, in);
+        }
+        if(legalEntity.getInn() == null) {
+            setInn(legalEntity, System.out, in);
+        }
+        if(legalEntity.getKpp() == null) {
+            setKpp(legalEntity, System.out, in);
+        }
+        if(legalEntity.getEntityType() == null) {
+            setType(legalEntity, System.out, in);
+        }
+        System.out.println(legalEntity);
+        System.out.println("press any to continue, x to exit...");
+        while (!in.nextLine().equals("x")) {
+            System.out.println("what do you want to change? \n1.ogrn \n2.inn \n3.kpp \n4.status\n5.name\nx - exit1");
+            String changeInput = in.nextLine();
+            switch (changeInput) {
+                case "1": setOgrn(legalEntity, System.out, in); break;
+                case "2": setInn(legalEntity, System.out, in); break;
+                case "3": setKpp(legalEntity, System.out, in); break;
+                case "4": setInn(legalEntity, System.out, in); break;
+                case "5": setName(legalEntity, System.out, in); break;
+            }
+            System.out.println(legalEntity);
+            System.out.println("press any to continue, x to exit...");
+        }
+        System.out.println("save changes? (y/n)");
+        switch (in.nextLine()) {
+            case "y": legalEntityRepository.save(legalEntity);
+            case "n":
+            default: break;
+        }
+//        C:\\Users\\bradi\\IdeaProjects\\xmltools\\src\\main\\resources
 //        legalEntityRepository.save(legalEntity);
-        val legalEntityGet = legalEntityRepository.getByOgrn(302032341L);
-        legalEntityGet.ifPresent(System.out::println);
+    }
+
+    private static void setOgrn(LegalEntity legalEntity, PrintStream out, Scanner in) {
+        out.println("enter ogrn: ");
+        legalEntity.setOgrn(Optional.of(in.nextLine()).map(Long::parseLong).orElse(0L));
+    }
+
+    private static void setInn(LegalEntity legalEntity, PrintStream out, Scanner in) {
+        out.println("enter inn: ");
+        legalEntity.setInn(Optional.of(in.nextLine()).orElse("777"));
+    }
+
+    private static void setKpp(LegalEntity legalEntity, PrintStream out, Scanner in) {
+        out.println("enter kpp: ");
+        legalEntity.setKpp(Optional.of(in.nextLine()).orElse("111"));
+    }
+
+    private static void setType(LegalEntity legalEntity, PrintStream out, Scanner in) {
+        out.println("enter type (organization, entrepreneur): ");
+        legalEntity.setEntityType(Optional.of(in.nextLine()).map(LegalEntity.EntityType::valueOf).orElse(LegalEntity.EntityType.organizaion));
+    }
+
+    private static void setStatus(LegalEntity legalEntity, PrintStream out, Scanner in) {
+        out.println("enter status (active, eliminated): ");
+        legalEntity.setStatus(Optional.of(in.nextLine()).map(LegalEntity.Status::valueOf).orElse(LegalEntity.Status.active));
+    }
+
+    private static void setName(LegalEntity legalEntity, PrintStream out, Scanner in) {
+        out.println("enter full name: ");
+        val name = Optional.ofNullable(legalEntity.getName()).orElse(new LegalEntity.Name());
+        name.setFullName(in.nextLine());
+        out.println("enter short name: ");
+        name.setShortName(in.nextLine());
+        legalEntity.setName(name);
     }
 }
--- src/main/java/ru/mirea/xmltools/domain/LegalEntity.java
+++ src/main/java/ru/mirea/xmltools/domain/LegalEntity.java
@@ -1,15 +1,14 @@
 package ru.mirea.xmltools.domain;
 
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.xml.bind.annotation.*;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-import javax.xml.datatype.XMLGregorianCalendar;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @XmlRootElement()
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -44,6 +43,8 @@ public class LegalEntity {
         organizaion, entrepreneur;
     }
 
+    @Data
+    @XmlAccessorType(XmlAccessType.FIELD)
     public static class Okved {
         @XmlElement()
         private String code;
@@ -53,6 +54,8 @@ public class LegalEntity {
         private String version;
     }
 
+    @Data
+    @XmlAccessorType(XmlAccessType.FIELD)
     public static class Founder {
         @XmlElement()
         private Long ogrn;
@@ -60,6 +63,8 @@ public class LegalEntity {
         private BigDecimal capitalPercent;
     }
 
+    @Data
+    @XmlAccessorType(XmlAccessType.FIELD)
     public static class Leader {
         @XmlElement()
         private Long ogrn;
@@ -67,6 +72,8 @@ public class LegalEntity {
         private String positionName;
     }
 
+    @Data
+    @XmlAccessorType(XmlAccessType.FIELD)
     public static class Name {
         @XmlElement()
         private String fullName;
@@ -118,13 +125,14 @@ public class LegalEntity {
 
     @Override
     public String toString() {
-        return this.entityType.toString().toUpperCase() + "\n" + this.name.fullName +
-                " (" + this.name.shortName + ")" +
-                "\nstatus:" + status +
-                "\nogrn:" + ogrn +
-                "; inn=" + inn +
-                "; kpp=" + kpp +
-                "\nfounders:" + founders +
-                "\nleaders:" + leaders;
+        return Optional.ofNullable(entityType).map(Object::toString).orElse("TYPE NOT SPECIFIED").toUpperCase() + "\n" +
+                Optional.ofNullable(this.name).map(Name::getFullName).orElse("FULL NAME NOT SPECIFIED") +
+                " (" + Optional.ofNullable(this.name).map(Name::getShortName).orElse("SHORT NAME NOT SPECIFIED") + ")" +
+                "\nstatus:" + Optional.ofNullable(status).map(Object::toString).orElse("NOT SPECIFIED") +
+                "\nogrn:" + Optional.ofNullable(ogrn).map(Object::toString).orElse("NOT SPECIFIED") +
+                "; inn=" + Optional.ofNullable(inn).map(Object::toString).orElse("NOT SPECIFIED") +
+                "; kpp=" + Optional.ofNullable(kpp).map(Object::toString).orElse("NOT SPECIFIED") +
+                "\nfounders:" + Optional.ofNullable(founders).map(Object::toString).orElse("NOT SPECIFIED") +
+                "\nleaders:" + Optional.ofNullable(leaders).map(Object::toString).orElse("NOT SPECIFIED");
     }
 }
