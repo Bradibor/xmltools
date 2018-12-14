868ac980f5ac93c6f6aab56626a866031e6fc3c3
bradibor <just1bomb@gmail.com>
2018-12-14 11:06:15 +0300

    custom

--- src/main/java/ru/mirea/xmltools/xmlprocessing/OrganizationService.java
+++ src/main/java/ru/mirea/xmltools/xmlprocessing/OrganizationService.java
@@ -2,6 +2,11 @@ package ru.mirea.xmltools.xmlprocessing;
 
 import ru.mirea.xmltools.domain.Organization;
 
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamSource;
 import java.io.*;
 import java.nio.file.Paths;
 import java.util.Optional;
@@ -10,6 +15,11 @@ public class OrganizationService {
     private Marshaller marshaller;
     private Unmarshaller unmarshaller;
     private String workingDir;
+    private File stylesheet;
+
+    public void setStylesheet(File stylesheet) {
+        this.stylesheet = stylesheet;
+    }
 
     public OrganizationService(Marshaller marshaller, Unmarshaller unmarshaller, String workingDir) {
         this.marshaller = marshaller;
@@ -28,6 +38,21 @@ public class OrganizationService {
         return Optional.empty();
     }
 
+    public Optional<Organization> getCustomByOgrn(Long ogrn) {
+        File file = Paths.get(workingDir, ogrn.toString() + ".xml").toFile();
+        try {
+            StreamSource styleSource = new StreamSource(stylesheet);
+            StreamSource dataSource = new StreamSource(new FileInputStream(file));
+            Transformer transformer = TransformerFactory.newInstance().newTransformer(styleSource);
+//            transformer.transform(dataSource, );
+            InputStream is = new FileInputStream(file);
+            return Optional.of(unmarshaller.unmarshall(is));
+        } catch (FileNotFoundException | TransformerConfigurationException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
     public void save(Organization organization) {
         File file = Paths.get(workingDir, organization.getOgrn().toString() + ".xml").toFile();
         try {
@@ -35,6 +60,17 @@ public class OrganizationService {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    public Optional<Organization> saveCustomByOgrn(Long ogrn) {
+        File file = Paths.get(workingDir, ogrn.toString() + ".xml").toFile();
+        try {
+            InputStream is = new FileInputStream(file);
+            return Optional.of(unmarshaller.unmarshall(is));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
     }
 
     public static void main(String[] args) {
